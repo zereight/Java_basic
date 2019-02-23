d9223f528a9561bdda7f68d7cd9a5f53a03f742a
zereight <aldwk68@naver.com>
2019-02-24 03:45:02 +0900

    BigInteger
    using BigInteger

--- Main.java
+++ Main.java
@@ -1,29 +1,12 @@
+import java.math.BigInteger;
 public class Main {
 
-    //final means "const"
-    final static double PI = 3.141592;
-    
     public static void main(String[] args) {
-        int int_num = 10;
-        double double_num = 1.9;
-        String String_text = "Hello!!";
-
-
-        //printIn is one-line, print is consecutive line.
-        System.out.println("type in: "+int_num);
-        System.out.print(double_num * PI + " and ");
-        System.out.println(String_text);
-
-        //bin, oct, hex using format
-        int a = 45;
-        System.out.format("%b \n", a);
-        System.out.format("%o \n", a);
-        System.out.format("%x \n", a);
-
-        //substring(x,y) : x<= index <y
-        String name = "abcdefghi";
-        System.out.println(name.substring(2, 5 ));
-        System.out.println(name.substring(0, name.length() ));
-
+        
+        BigInteger a = new BigInteger("13782179392173273791223892183021");
+        BigInteger b = new BigInteger( "1238754875847539483948394839" );
+        BigInteger c = a.add(b);
+        BigInteger d = c.multiply(a);
+        System.out.println(d);
     }
 }
