6e3753bd8b0a64a647d7f853137b12c7da5add5d
zereight <aldwk68@naver.com>
2019-02-24 03:30:13 +0900

    2. print, format, substring, bin-oct-hex
    print & format / String-substring,length() / format-oct,hex,bin

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 public class Main {
+
+    //final means "const"
+    final static double PI = 3.141592;
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int int_num = 10;
+        double double_num = 1.9;
+        String String_text = "Hello!!";
+
+
+        //printIn is one-line, print is consecutive line.
+        System.out.println("type in: "+int_num);
+        System.out.print(double_num * PI + " and ");
+        System.out.println(String_text);
+
+        //bin, oct, hex using format
+        int a = 45;
+        System.out.format("%b \n", a);
+        System.out.format("%o \n", a);
+        System.out.format("%x \n", a);
+
+        //substring(x,y) : x<= index <y
+        String name = "abcdefghi";
+        System.out.println(name.substring(2, 5 ));
+        System.out.println(name.substring(0, name.length() ));
+
     }
 }
