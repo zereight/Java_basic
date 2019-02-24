8ae074e8c560a97516635e4cef299a89a75d272c
zereight <aldwk68@naver.com>
2019-02-24 11:23:39 +0900

    7. File io

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    //File io
     public static void main(String[] args) {
         
-        //User input
-        Scanner sc1 = new Scanner(System.in);
-        System.out.print("input integer: ");
-
-        int i = sc1.nextInt();
-        System.out.println(i);
-
-        sc1.close();
-
+        File file = new File("input.txt");
+        try {
+            Scanner sc  = new Scanner(file);
+            while( sc.hasNext() ){
+                System.out.println( sc.nextInt() );
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+            
         
 
     }
