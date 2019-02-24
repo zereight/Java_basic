e1abc99fd002dd857253c3ecf2e6bce3b65f0924
zereight <aldwk68@naver.com>
2019-02-24 11:55:17 +0900

    7. Function/ callByValue,Reference
    Java basically has not CallByRefernce, but if you modify object property using '.'operator it will be operated like CallByReference.

--- Main.java
+++ Main.java
@@ -1,23 +1,46 @@
-import java.io.*;
-import java.util.*;
 
 public class Main {
 
-    //File io
-    public static void main(String[] args) {
-        
-        File file = new File("input.txt");
-        try {
-            Scanner sc  = new Scanner(file);
-            while( sc.hasNext() ){
-                System.out.println( sc.nextInt() );
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-        }
-            
-        
-
+    public static int max(int a, int b, int c) {
+        int sum = a+b+c;
+        return sum;
     }
+
+    public static void main (String[] args) {
+
+        //func
+        System.out.println( max(10,20,30) );
+
+
+        //java func callBy???
+        Person p1 = new Person("A");
+        Person p2 = new Person("B");
+
+        Person.callByValue(p1); //no change
+        Person.callByReference(p2); // change
+
+        Person.print_Person(p1);
+        Person.print_Person(p2);
+    }
+}
+
+class Person {
+    String name;
+
+    public Person(String _name){
+        this.name = _name;
+    }
+
+    public static void callByValue(Person _p){
+        _p = new Person("a");
+    }
+
+    public static void callByReference(Person _p){
+        _p.name = "b";
+    }
+
+    public static void print_Person(Person _p){
+        System.out.println(_p.name);
+    }
+
 }
