219ddd348e7d2f5966d65a7904d748f213b981ed
zereight <aldwk68@naver.com>
2019-02-25 12:23:27 +0900

    12. Abstract
    class which inherited abstract class must define that abstract property.

---
+++ Animal.java
@@ -0,0 +1,4 @@
+abstract class Animal {
+    abstract void Crying();
+    abstract void Kill();
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        Dog dog1 = new Dog("kkk", true);
+        dog1.Crying();
+        dog1.Kill();
+    }
+}
