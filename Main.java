0fc4e699396afa74e7014a0b8f46b8717bd374f9
zereight <aldwk68@naver.com>
2019-02-25 10:50:36 +0900

    8. Array

--- Main.java
+++ Main.java
@@ -1,46 +1,20 @@
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
 
-    public static int max(int a, int b, int c) {
-        int sum = a+b+c;
-        return sum;
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        for(int i=0; i<10; i++){
+            arr[i] = i+100;
+        }
+        
+        for (int i =0 ; i<arr.length; i++) {
+            System.out.println(i);
+        }
+        
+        
     }
-
-    public static void main (String[] args) {
-
-        //func
-        System.out.println( max(10,20,30) );
-
-
-        //java func callBy???
-        Person p1 = new Person("A");
-        Person p2 = new Person("B");
-
-        Person.callByValue(p1); //no change
-        Person.callByReference(p2); // change
-
-        Person.print_Person(p1);
-        Person.print_Person(p2);
-    }
-}
-
-class Person {
-    String name;
-
-    public Person(String _name){
-        this.name = _name;
-    }
-
-    public static void callByValue(Person _p){
-        _p = new Person("a");
-    }
-
-    public static void callByReference(Person _p){
-        _p.name = "b";
-    }
-
-    public static void print_Person(Person _p){
-        System.out.println(_p.name);
-    }
-
 }
